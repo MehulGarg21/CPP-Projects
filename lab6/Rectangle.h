0b37afbfb3e6186341d210ec7bb02a8e08fe1ec7
MehulGarg21 <darshisgid@gmail.com>
2025-09-14 11:03:02 -0700

    Update Rectangle.h

--- lab6/Rectangle.h
+++ lab6/Rectangle.h
@@ -9,46 +9,5 @@
 //  the University of Toronto. It is prohibited to distribute
 //  this code, either publicly or to third parties.
 
-#ifndef Rectangle_h
-#define Rectangle_h
-
-#include <iostream>
-#include <string>
-using namespace std;
-#include "Shape.h"
-
-
-// This class implements the rectangle Shape objects.
-// ECE244 Student: Write the definition of the class here.
-class Rectangle : public Shape {
-private:
-    float width,height;                
-
-public:
-    // Constructor
-    Rectangle (string n, float xcen, float ycen,float w,float h);
-    
-    // Destructor
-    virtual ~Rectangle();
-    
-    // Accessor
-    float getWidth() const;   
-    float getHeight() const; 
-    
-    // Mutator
-    void setHeight(float x1);     
-    void setWidth(float x2); 
-
-    // Utility methods
-    virtual void draw() const;     // Draws the triangle; for the assignment it
-                                   // prints the information of the Rectangle
-    
-    virtual float computeArea() const;   // Computes the area of the Rectangle
-    
-    virtual Shape* clone() const;  // Clones the object
-};
-
-
-#endif /* Rectangle_h */
 
 
